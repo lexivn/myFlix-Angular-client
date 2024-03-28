89082eaeb3b2bf1adfd778c8ff76a363c2f54f92
Alecks <msae26@hotmail.com>
2024-03-28 18:39:52 -0400

    Fixed Login fetch
    It was added the feature to set LocalStorage with the user and token.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -15,6 +16,14 @@ export class AppComponent {
   // when the signup button is clicked
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent,
+      {
+        // Assigning the dialog a width
+        width: '280px'
+      });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent,
       {
         // Assigning the dialog a width
         width: '280px'
